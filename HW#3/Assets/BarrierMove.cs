14f600889072cdf22bebee2b6d784e68e9a7886d
ezgy <ezgibati@gmail.com>
2022-03-26 20:07:18 +0300

    added movement to barriers

--- HW#3/Assets/BarrierMove.cs
+++ HW#3/Assets/BarrierMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BarrierMove : MonoBehaviour
 {
+    private float duration = 1f;
+    private bool isdown = true;
+    private Vector3 aimedPos; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,38 @@ public class BarrierMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        duration = Random.Range(0.5f, 5f);
+        Vector3 move = transform.position;
+        Vector3 start = transform.position;
+        if (isdown)
+        {
+            aimedPos = new Vector3(0,9,0);
+            while (transform.position.y < aimedPos.y)
+            {
+                print(aimedPos.y);
+                move.y += 0.2f;
+                transform.position = move;
+                yield return new WaitForSecondsRealtime(5);
+            }
+        }
+        else
+        {
+            aimedPos = new Vector3(0, 3, 0);
+            while (transform.position.y > aimedPos.y)
+            {
+                print(aimedPos.y);
+                move.y -= 0.2f;
+                transform.position = move;
+                yield return new WaitForSecondsRealtime(5);
+            }
+        }
+        isdown = !isdown;
+        //transform.position = Vector3.MoveTowards(transform.position, aimedPos, 0.5f);
+        yield return new WaitForSeconds(duration*10);
     }
 }
